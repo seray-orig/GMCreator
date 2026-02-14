570baa473684b287e065d942adce04930ae78d77
seray-orig <serayplayplay@gmail.com>
2026-02-14 22:40:41 +0300

    test

---
+++ GMCreator.GLua/LuaArgument.cs
@@ -0,0 +1,14 @@
+﻿namespace GMCreator.GLua;
+
+public struct LuaArgument
+{
+    public string Value;
+
+    public LuaArgument(string value, bool variable)
+    {
+        if (variable)
+            Value = value;
+        else
+            Value = ;
+    }
+}
