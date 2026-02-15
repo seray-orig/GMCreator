4e49f5e35f6476ffa294b81df6b712ba94e6c6e9
seray-orig <serayplayplay@gmail.com>
2026-02-16 00:07:24 +0300

    Welcome to Avalonia! (Переезд на дотнет 10 и Авалонию)

---
+++ GMCreator.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GMCreator.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
