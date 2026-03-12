142abb2ddeb8956b18bf1eeaca627e4d758728e5
seray-orig <serayplayplay@gmail.com>
2026-03-12 13:54:33 +0300

    test

--- GMCreator.GLua/LuaCodeBlock.cs
+++ GMCreator.GLua/LuaCodeBlock.cs
@@ -2,29 +2,13 @@
 
 namespace GMCreator.GLua;
 
-public class LuaCodeBlock
+public class LuaCodeBlock(string countour, string filler)
 {
-    public string Header = String.Empty;
-    public string Code;
-    public string Ending = String.Empty;
-
-    public LuaCodeBlock(string code)
-    {
-        Code = code;
-    }
-
-    public LuaCodeBlock(string header, string code, string ending = "end")
-    {
-        Header = header;
-        Code = code;
-        Ending =  ending;
-    }
+    public string Сontour = countour;
+    public string Filler = filler;
     
     public override string ToString()
     {
-        if(Header == String.Empty)
-            return Code;
-
         var tempString = new StringBuilder(Code.Length);
         tempString.Append($"{Header}\n");
         foreach(var line in Code.Split([ "\r\n", "\r", "\n" ], StringSplitOptions.None))
